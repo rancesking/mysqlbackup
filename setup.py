54d64809b4c383babad954106ddde12fbf9d8352
Racosta <rances.acosta21@gmail.com>
2023-11-24 14:23:35 -0400

    First Commit.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Rances Acosta',
+    author_email='rances.acosta21@gmail.com',
+    description='An utility for backing up PostgreSQL databases.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rancesking/pgbackup',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main'
+        ], 
+    }
+
+)
